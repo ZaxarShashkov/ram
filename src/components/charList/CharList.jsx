8fe0fd194a6e2b7cd4a003ba4a47b602147f336f
ZaxarShashkov <zekharjah@gmail.com>
2023-03-26 19:07:30 +0300

    adaptive

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import ErrorMessage from "../errorMessage/ErrorMessage";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
+import "../../style/button.scss";
 import "./charList.scss";
 import RamApi from "../../services/RamApi";
 import Spinner from "../spinner/Spinner";
 
 class CharList extends Component {
+  constructor(props) {
+    super(props);
+    this.toTop = React.createRef();
+  }
 
   state = {
     charList: [],
@@ -14,60 +19,69 @@ class CharList extends Component {
     error: false,
     newItemLoading: false,
     page: 1,
-    autoLoad: true
+    autoLoad: true,
   };
 
   ramApi = new RamApi();
 
+  onTop = () => {
+    console.log("totop");
+    this.toTop.current.scrollIntoView();
+  };
+
   scrollHandler = (e) => {
-    if(e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100) {
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) <
+      20
+    ) {
       return this.onRequest(this.state.page);
-    }  
-  }
+    }
+  };
 
   onLoad = () => {
     this.setState({
-      autoLoad: !this.state.autoLoad
-    })
-  }
+      autoLoad: !this.state.autoLoad,
+    });
+  };
 
   removeScrollHandler = () => {
     this.onLoad();
-    window.removeEventListener('scroll', this.scrollHandler);
-  }
+    window.removeEventListener("scroll", this.scrollHandler);
+  };
 
   componentDidMount() {
-    window.addEventListener('scroll', this.scrollHandler)
+    window.addEventListener("scroll", this.scrollHandler);
     this.ramApi
-        .getAllCharacters()
-        .then(this.onCharListLoaded)
-        .catch(this.onError);
+      .getAllCharacters()
+      .then(this.onCharListLoaded)
+      .catch(this.onError);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', this.scrollHandler)
+    window.removeEventListener("scroll", this.scrollHandler);
   }
 
   onRequest = (page) => {
     this.onCharListLoading();
     this.ramApi
-        .getAllCharacters(page)
-        .then(this.onCharListLoaded)
-        .catch(this.onError);
-  }
+      .getAllCharacters(page)
+      .then(this.onCharListLoaded)
+      .catch(this.onError);
+  };
 
   onCharListLoading = () => {
     this.setState({
-      newItemLoading: true
-    })
-  }
+      newItemLoading: true,
+    });
+  };
 
   onCharListLoaded = (newCharList) => {
-    this.setState(({page , charList}) => ({
-        charList: [...charList, ...newCharList],
-        loading: false,
-        newItemLoading: false,
-        page: page + 1,
+    this.setState(({ page, charList }) => ({
+      charList: [...charList, ...newCharList],
+      loading: false,
+      newItemLoading: false,
+      page: page + 1,
     }));
   };
 
@@ -77,7 +91,6 @@ class CharList extends Component {
       loading: false,
     });
   };
-  
 
   renderItems(arr) {
     const items = arr.map((item) => {
@@ -86,58 +99,66 @@ class CharList extends Component {
           className="char__item"
           key={item.id}
           onClick={() => this.props.onCharSelected(item.id)}
+        >
+          <div
+            className="char__block"
+            onClick={() => this.props.setModal(this.props.visible)}
+            visible={this.props.visible}
           >
-          <div className="char__block"
-          onClick={() => this.props.setModal(this.props.visible)}
-          visible={this.props.visible}>
             <img src={item.image} alt="char" className="char__img" />
           </div>
           <h6 className="char__name">{item.name}</h6>
         </li>
       );
     });
-    return (
-        <ul className="char__grid">
-            {items}
-        </ul>
-    )
+    return <ul className="char__grid">{items}</ul>;
   }
 
   render() {
-    const { charList, loading, error, newItemLoading , page } = this.state;
+    const { charList, loading, error, newItemLoading, page } = this.state;
     const items = this.renderItems(charList);
 
     const errorMessage = error ? <ErrorMessage /> : null;
     const spinner = loading ? <Spinner /> : null;
     const content = !(loading || error) ? items : null;
 
-    if(this.state.autoLoad) {
-      window.addEventListener('scroll', this.scrollHandler);
+    if (this.state.autoLoad) {
+      window.addEventListener("scroll", this.scrollHandler);
     }
 
     return (
-      <div className="char__list">
+      <div className="char__list" ref={this.toTop}>
         {errorMessage}
         {spinner}
         {content}
-        <button style={{ marginTop: "3em" }}
-                disabled={newItemLoading}
-                onClick={() => this.onRequest(page)}
-                >
-                Load More
-        </button>
-        <button className="remove"
-        style={{ marginBottom : "20em" }}
-        onClick={() => this.removeScrollHandler()}
-                >remove</button>
+        <div className="char__list_click">
+          <button
+            className="char__list_load char__list_btn"
+            disabled={newItemLoading}
+            onClick={() => this.onRequest(page)}
+          >
+            Load More
+          </button>
+          <button
+            className="char__list_auto char__list_btn"
+            onClick={() => this.removeScrollHandler()}
+          >
+            Auto Load
+          </button>
+          <button
+            className="char__list_scroll char__list_btn"
+            onClick={this.onTop}
+          >
+            To Top
+          </button>
+        </div>
       </div>
     );
   }
 }
 
-
 CharList.propTypes = {
-  onCharSelected: PropTypes.func.isRequired
-}
+  onCharSelected: PropTypes.func.isRequired,
+};
 
 export default CharList;
